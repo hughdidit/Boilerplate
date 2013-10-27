84ead54158a8ecb90f16b7985db8afac3a181625
Hugh Chapman <hughdidit@gmail.com>
2013-10-27 10:35:02 -0700

    New PHP Class Boilerplate

---
+++ Class_Boilerplate.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *  Class Boilerplate
+ *
+ *  description 
+ *
+ *  https://github.com/hughdidit/PackageName
+ *
+ *  @package    PackageName
+ *  @author     Hugh Chapman
+ *  @copyright  (c) 2013 - Hugh Chapman
+ *  @version    1.0.0
+ */
+class NewClass {
+    
+	
+    public $public = 30;       
+    
+    private $private = '';
+
+    const CONSTANT_NAME = 'constant value';
+    
+ 	/**
+	*	Constructor function
+	*
+	*/
+	
+	function NewClass() {
+		// New class constructor. Note it has the same naem as Class
+		
+	}
+ 	
+   	/**
+     *  Protected Function Declaration
+     *
+     *  Describe your protected function here
+     *   
+     */    
+    protected function protFunc() {
+        //protFunc process
+
+    }
+   
+	
+	 /**
+     *  Private Function Declaration
+     *
+     *  Describe your private function here
+     *   
+     */    
+    private function privFunc() {
+        //privFunc process
+
+    }
+   
+    
+    /**
+     *  Public Function Declaration
+     *
+     *  Describe your public function here
+     *   
+     */    
+    public function pubFunc() {
+        //pubFunc process
+
+    }
+}
+
